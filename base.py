7bb5d1f046e4ec86f34f191b52fed60d3aa8a1c2
UniqWc <50565660+UniqWc@users.noreply.github.com>
2023-02-07 14:44:02 +0800

    update base config

--- base.py
+++ base.py
@@ -1,19 +1,19 @@
-key = "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"  ### 这里要填上你自己的API,或者把API加入系统的环境变量。
+key = ""  ### 这里要填上你自己的API,或者把API加入系统的环境变量。
 
 rabbit_test_DATABASES = {
-    "HOST": "172.16.100.57",
+    "HOST": "",
     "PORT": 3306,
     "USER": "root",
-    "PASSWORD": "sherpa.com.cn",
+    "PASSWORD": "",
     "DB": "rabbit",
     "CHARSET": "utf8",
 }
 
 rabbit_prod_DATABASES = {
-    "HOST": "172.16.100.57",
+    "HOST": "",
     "PORT": 3306,
     "USER": "root",
-    "PASSWORD": "sherpa.com.cn",
+    "PASSWORD": "",
     "DB": "rabbit",
     "CHARSET": "utf8",
 }
